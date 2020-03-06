953b429da76df58655ebc2f6f7dbc9bfc804d42f
TK <leandrotk100@gmail.com>
2020-03-05 22:34:56 -0300

    types and classes intro

---
+++ introduction-to-functional-programming/02.types-and-classes/intro.hs
@@ -0,0 +1,13 @@
+-- A type is a name for a collection of related values
+-- Types in Haskell
+-- the value `e` with type `t` looks like `e :: t`
+
+-- Basic Types: Bool, Char, String, Int, Integer, Bool, Float
+-- name :: String
+
+-- List Types: a sequence of values of the same type
+-- [False, True, True, False] :: [Bool]
+-- ['a', 'b', 'c', 'd'] :: [Char]
+
+-- Tuple Types: sequence of value with different types
+-- (True, 'a', False) :: (Bool, Char, Bool)
