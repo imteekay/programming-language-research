1ec92f17d99bc272f8d2dd4e6ed2850cbf269bdd
TK <leandrotk100@gmail.com>
2020-03-09 22:57:52 -0300

    fibonacci recursive and iterative

--- sicp/building-abstractions-with-procedures/procedures-and-the-processes-they-generate.scm
+++ sicp/building-abstractions-with-procedures/procedures-and-the-processes-they-generate.scm
@@ -17,3 +17,19 @@
       (fact-iter (* product counter)
                  (+ counter 1)
                  max-count)))
+
+; it is a terrible way to compute Fibonacci numbers
+; because it does so much redundant computation.
+(define (fib n)
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        (else (+ (fib (- n 1))
+                 (fib (- n 2))))))
+
+(define (fib n)
+  (fib-ite 0 1 n))
+
+(define (fib-iter a b count)
+  (if (= count 0)
+    a
+    (fib-iter b (+ a b) (- count 1)))
