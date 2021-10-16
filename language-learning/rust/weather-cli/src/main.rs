2a6bf899cff3be55d9bd26f500a24fdb336f7f5a
TK <leandrotk100@gmail.com>
2021-10-16 19:08:11 -0300

    using iterators to get arguments in the cli

--- language-learning/rust/weather-cli/src/main.rs
+++ language-learning/rust/weather-cli/src/main.rs
@@ -1,5 +1,10 @@
 fn main() {
     let api_token = std::env::var("API_TOKEN")
         .expect("expected there to be an api token");
-    dbg!(api_token);
+
+    let mut arg_iterator = std::env::args();
+    arg_iterator.next();
+    let args: String = arg_iterator.collect();
+
+    dbg!(args);
 }
