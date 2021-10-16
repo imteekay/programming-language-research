0a4165939c61e5db01a9b05f7213513e17c922b5
TK <leandrotk100@gmail.com>
2021-10-16 17:05:42 -0300

    unsound

---
+++ language-learning/typescript/unsound.ts
@@ -0,0 +1,21 @@
+// A sound type system only accepts programs with correct types. TypeScript's type system is unsound:
+let names: string[] = ["Amir", "Betty"];
+let unsoundNames: (string | number)[] = names;
+
+// This is unsound! It causes the "names" array to contain a number!
+unsoundNames.push(5);
+names; // ['Amir', 'Betty', 5]
+
+// Other example with undefined value while the type is a string
+const blas: string[] = ["Amir", "Betty"];
+const bla: string = blas[2];
+bla; // undefined
+
+// With any, anything can be assigned to anything. No type error:
+const n: any = 5;
+const s: string = n;
+s; // => 5
+
+const user: any = { name: "Amir" };
+const numbers: number[] = user;
+numbers; // => {name: 'Amir'}
